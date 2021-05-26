33497a0411ad63459c4cf287e9f8d79245ca2319
passy4ucj <ojinnnakapascal@gmail.com>
2021-05-26 11:36:47 +0100

    Middleware and entry file  (server.js)

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const { sequelize } = require('./models')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+
+
+// Creating an express app
+const app = express()
+
+// Initializing dotenv
+//Load env vars
+dotenv.config({ path: './config/env.env' })
+
+// Route files
+
+
+// Using JSON parser
+app.use(express.json())
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Mount routers
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    })
+})
+
+
+//Use error Middleware
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+// App Listener
+app.listen(PORT, async () => {
+    console.log(`App Listening on PORT ${PORT}`)
+    await sequelize.authenticate()
+    console.log('Database Connected!')
+});
