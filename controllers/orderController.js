cc14a3670c1c2307f95d0ace51715c41941a2cdd
passy4ucj <ojinnnakapascal@gmail.com>
2021-05-28 22:51:06 +0100

    Get, update and Delete orders controller and routes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,14 +2,6 @@ const { Order, OrderItems } = require('../models')
 const asyncHandler = require('express-async-handler')
 const { uuid } = require('uuidv4')
 
-
-// var numbers = [65, 44, 12, 4];
-// numbers.forEach(myFunction)
-
-// function myFunction(item, index, arr) {
-//   arr[index] = item * 10;
-// }
-
 const createOrder = asyncHandler(async (req, res) => {
     const {
         orderItemss,
@@ -21,17 +13,6 @@ const createOrder = asyncHandler(async (req, res) => {
     const uuidID = uuid()
 
 
-    // const addItems = (item) => {
-    //     const added = await OrderItems.create({
-    //         pizzaType: item.pizzaType,
-    //         size: item.size,
-    //         quantity: item.quantity,
-    //         subTotal: item.subTotal,
-    //         order_uuid: uuidID
-    //     })
-    //     return added;
-    // }
-
     try {
         const order = await Order.create({
             orderID: uuidID,
@@ -40,9 +21,6 @@ const createOrder = asyncHandler(async (req, res) => {
             status,
             totalAmount
         })
-        // const itemss = OrderItems.bulkCreate(orderItemss)
-
-        // numbers.forEach(myFunction)
 
         orderItemss.map(async(item) => {
             await OrderItems.create({
@@ -70,8 +48,73 @@ const createOrder = asyncHandler(async (req, res) => {
 
 })
 
+const updateOrder = asyncHandler(async (req, res) => {
+
+    const {
+        status
+    } = req.body
+
+    try {
+        const updatedOrder = await Order.update({ status }, {
+            where: {
+                orderID: req.params.id
+            }
+          });
+    
+        res.json({
+            success: true,
+            updatedOrder
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+        throw new Error(`Server error`)
+    }
+    
+  
+})
+
+const getOrders = asyncHandler(async (req, res) => {
+    try {
+        const orders = await Order.findAll({})
+
+        res.status(200).json({
+            success: true,
+            orders
+        })
+    } catch (error) {
+        res.status(500)
+        throw new Error('No orders')
+    }
+})
+
+const deleteOrder = asyncHandler(async (req, res) => {
+    try {
+        await Order.destroy({
+            where: {
+                orderID: req.params.id
+            }
+        })
+
+        await OrderItems.destroy({
+            where: {
+                order_uuid: req.params.id
+            }
+        })
+
+        res.json({
+            success: true
+        })
+    } catch (error) {
+        
+    }
+})
+
 
 
 module.exports = {
-    createOrder
+    createOrder,
+    updateOrder,
+    getOrders,
+    deleteOrder
 }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const { createOrder } = require('../controllers/orderController')
+const { createOrder, updateOrder, getOrders, deleteOrder } = require('../controllers/orderController')
 
 const router = express.Router()
 
 
 router.route('/')
     .post(createOrder)
+    .get(getOrders)
+
+router.route('/:id')
+    .put(updateOrder)
+    .delete(deleteOrder)
 
 module.exports = router
